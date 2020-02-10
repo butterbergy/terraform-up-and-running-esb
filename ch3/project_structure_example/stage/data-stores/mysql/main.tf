9f3db06dc6b2f51b4da10f8c1ab26c53042b94c9
Eric Berglund <ericsamuelberglund@gmail.com>
2020-02-09 23:04:16 -0700

    Move bash out of tf and into sh file, re-org output vars

--- ch3/project_structure_example/stage/data-stores/mysql/main.tf
+++ ch3/project_structure_example/stage/data-stores/mysql/main.tf
@@ -23,12 +23,3 @@ terraform {
     encrypt        = true
   }
 }
-
-output "address" {
-  value       = aws_db_instance.example.address
-  description = "Connect to the database at this endpoint"
-}
-output "port" {
-  value       = aws_db_instance.example.port
-  description = "The port the database is listening on"
-}
--- ch3/project_structure_example/stage/services/webserver-cluster/main.tf
+++ ch3/project_structure_example/stage/services/webserver-cluster/main.tf
@@ -8,13 +8,7 @@ resource "aws_launch_configuration" "example" {
   image_id        = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
-  user_data       = <<EOF
-                    #!/bin/bash
-                    echo "Hello, World" >> index.html
-                    echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
-                    echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
-                    nohup busybox httpd -f -p ${var.server_port} &
-                    EOF
+  user_data       = data.template_file.user_data.rendered
   # Required when using a launch configuration with an auto scaling group.
   # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
   lifecycle {
@@ -151,5 +145,14 @@ data "terraform_remote_state" "db" {
     bucket = "terraform-up-and-running-state-esb"
     key    = "stage/data-stores/mysql/terraform.tfstate"
     region = "us-east-2"
+  }
+}
+
+data "template_file" "user_data" {
+  template = file("user-data.sh")
+  vars = {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
   }
 }
