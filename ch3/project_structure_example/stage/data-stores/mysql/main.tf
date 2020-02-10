af0eb2fdffc905226bc4ac7b68092991bffb7531
Eric Berglund <ericsamuelberglund@gmail.com>
2020-02-09 22:41:10 -0700

    Add mysql server, use clean project structure

---
+++ ch3/project_structure_example/stage/data-stores/mysql/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_db_instance" "example" {
+  identifier_prefix = "terraform-up-and-running"
+  engine            = "mysql"
+  allocated_storage = 10
+  instance_class    = "db.t2.micro"
+  name              = "example_database"
+  username          = "admin"
+  # How should we set the password?
+  password = var.db_password
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "terraform-up-and-running-state-esb"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+output "address" {
+  value       = aws_db_instance.example.address
+  description = "Connect to the database at this endpoint"
+}
+output "port" {
+  value       = aws_db_instance.example.port
+  description = "The port the database is listening on"
+}
---
+++ ch3/project_structure_example/stage/services/webserver-cluster/var.tf
@@ -0,0 +1,6 @@
+# Server port input var
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
