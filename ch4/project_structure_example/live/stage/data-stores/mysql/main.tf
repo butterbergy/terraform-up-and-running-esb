9bbbbf0753116d50e467d781d22b0ff6a3b205cb
Eric Berglund <ericsamuelberglund@gmail.com>
2020-02-10 21:53:30 -0700

    terraform fmt

--- ch4/project_structure_example/live/stage/data-stores/mysql/main.tf
+++ ch4/project_structure_example/live/stage/data-stores/mysql/main.tf
@@ -15,12 +15,12 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "example" {
-  identifier_prefix = "terraform-up-and-running"
-  engine            = "mysql"
-  allocated_storage = 10
-  instance_class    = "db.t2.micro"
-  name              = var.db_name
-  username          = "admin"
-  password = var.db_password
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  name                = var.db_name
+  username            = "admin"
+  password            = var.db_password
   skip_final_snapshot = true
 }
